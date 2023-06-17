81855c83b1facef45f1e0b552e2305a17012d63b
abhirrajput210 <abhishekrajput.lamprostech@gmail.com>
2023-06-17 21:21:35 +0530

    resolved folder naming issue

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar/Navbar";
+import Navbar from "./components/navbar/Navbar";
 import HomePage from "./pages/HomePage";
 import BecomeMemberPage from "./pages/BecomeMemberPage";
 // import Card from "./components/voting/Card";
